6b49efaf2479f99889de555425e31a9b68506dc5
Star <i@ssstttar.com>
2024-08-01 20:13:19 +0800

    fix(easylpac): sha256 error

--- Casks/easylpac.rb
+++ Casks/easylpac.rb
@@ -2,8 +2,8 @@ cask "easylpac" do
   arch arm: "arm64", intel: "x86_64"
 
   version "0.7.6.2"
-  sha256 arm: "93df28ab4e350b1e3e9149026cb2b14149e3a2768b3189b9d426790eb6e7c17c",
-         intel: "2e615cf715c751ee815c3473e612f57ea8e5576b20857386feebaefbe9228c41"
+  sha256 arm: "0a3c84b1837bfa7ad74358c11b292c76e317e5815c93593fcf7ef4496c721d59",
+         intel: "2d127c29d8f1ebd8cd1bdaea6ae3a84dad606e3eefbc66399b996fce2e641d41"
 
   url "https://github.com/creamlike1024/EasyLPAC/releases/download/#{version}/EasyLPAC-macOS-#{arch}-with-lpac.dmg"
   name "EasyLPAC"
