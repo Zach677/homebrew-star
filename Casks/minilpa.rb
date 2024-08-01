25aeef2c155b528e98acfcd47bdee951a67c8c4c
Star <i@ssstttar.com>
2024-08-01 20:46:55 +0800

    fix minilpa url
    Signed-off-by: Star <i@ssstttar.com>

--- Casks/minilpa.rb
+++ Casks/minilpa.rb
@@ -5,7 +5,7 @@ cask "minilpa" do
   sha256 arm:   "C40D23CD6BE72C36757CAC883F1EBA27AEA8DADA1D67334B9A365791C281CD9A",
          intel: "E130FB021ACA5794CBA7E676099E390F8C5179F75711B3AD33AA5DBA469A3A43"
 
-  url "https://github.com/creamlike1024/EasyLPAC/releases/download/v#{version}/MiniLPA-macOS-#{arch}.dmg"
+  url "https://github.com/EsimMoe/MiniLPA/releases/download/v#{version}/MiniLPA-macOS-#{arch}.dmg"
   name "MiniLPA"
   desc "Professional LPA UI"
   homepage "https://github.com/EsimMoe/MiniLPA"
