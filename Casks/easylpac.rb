eaabd71171c1a8b39952eda4c7dcd40cefda589b
Star <i@ssstttar.com>
2024-08-02 21:42:32 +0800

    update
    Signed-off-by: Star <i@ssstttar.com>

--- Casks/easylpac.rb
+++ Casks/easylpac.rb
@@ -1,7 +1,7 @@
 cask "easylpac" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.7.6.3"
+  version "0.7.6.2"
   sha256 arm:   "93df28ab4e350b1e3e9149026cb2b14149e3a2768b3189b9d426790eb6e7c17c",
          intel: "2e615cf715c751ee815c3473e612f57ea8e5576b20857386feebaefbe9228c41"
 
