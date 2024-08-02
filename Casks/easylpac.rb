1652b1ee4785e728bbd6c3d42cc0e923d065a195
Star <i@ssstttar.com>
2024-08-02 22:01:59 +0800

    fix
    Signed-off-by: Star <i@ssstttar.com>

--- Casks/easylpac.rb
+++ Casks/easylpac.rb
@@ -1,9 +1,9 @@
 cask "easylpac" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.7.6.2"
-  sha256 arm:   "93df28ab4e350b1e3e9149026cb2b14149e3a2768b3189b9d426790eb6e7cc",
-         intel: "2e615cf715c751ee815c3473e612f57ea8e5576b20857386feebaefb28c41"
+  version "0.7.6.3"
+  sha256 arm:   "93df28ab4e350b1e3e9149026cb2b14149e3a2768b3189b9d426790eb6e7c17c",
+         intel: "2e615cf715c751ee815c3473e612f57ea8e5576b20857386feebaefbe9228c41"
 
   url "https://github.com/creamlike1024/EasyLPAC/releases/download/#{version}/EasyLPAC-macOS-#{arch}-with-lpac.dmg"
   name "EasyLPAC"
