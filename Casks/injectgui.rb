dc0bfcb209eab248fd96ecdf7b30abda7d96b6e3
Star <i@ssstttar.com>
2024-08-03 20:05:05 +0800

    fix
    Signed-off-by: Star <i@ssstttar.com>

--- Casks/injectgui.rb
+++ Casks/injectgui.rb
@@ -5,7 +5,7 @@ cask "injectgui" do
   url "https://github.com/wibus-wee/InjectGUI/releases/download/v#{version}/InjectGUI.dmg"
   name "InjectGUI"
   desc "macOS Integrated Injection Framework (GUI version)"
-  homepage "https://github.com/wibus-wee/InjectGUI/tree/main"
+  homepage "https://github.com/wibus-wee/InjectGUI"
 
   livecheck do
     url :url
