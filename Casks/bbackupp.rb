2e14fb320225e4972fc9d4e1159ee264c949744e
Zach <i@ssstttar.com>
2024-08-06 17:48:49 +0800

    fix: BBackupp file name
    Signed-off-by: Zach <i@ssstttar.com>

--- Casks/bbackupp.rb
+++ Casks/bbackupp.rb
@@ -2,7 +2,7 @@ cask "bbackupp" do
   version "2.12.66"
   sha256 "903ccca8d240e43289536b20e6b5d45799608692c3192fd48e37f80e45039c33"
 
-  url "https://github.com/Lakr233/BBackupp/releases/download/#{version}/BBackupp.app.zip"
+  url "https://github.com/Lakr233/BBackupp/releases/download/#{version}/BBackupp.zip"
   name "BBackupp"
   desc "Automated iOS Backup Robot"
   homepage "https://github.com/Lakr233/BBackupp"
