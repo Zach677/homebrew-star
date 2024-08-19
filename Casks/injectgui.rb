e4833ec1cea0931f092223a6d4d1d2930ee1b042
Zach <i@ssstttar.com>
2024-08-19 14:58:41 +0800

    fix: injectgui

--- Casks/injectgui.rb
+++ Casks/injectgui.rb
@@ -2,7 +2,7 @@ cask "injectgui" do
   version "1.2.0"
   sha256 "ee6ae1ce40b1f1e8b09ef27c1f786eaf73660dd02d3c4c78600db3ec7115831f"
 
-  url "https://github.com/wibus-wee/InjectGUI/releases/download/v#{version}/InjectGUI.dmg"
+  url "https://github.com/wibus-wee/InjectGUI/releases/download/v.#{version}/InjectGUI.dmg"
   name "InjectGUI"
   desc "Integrated Injection Framework (GUI version)"
   homepage "https://github.com/wibus-wee/InjectGUI"
