87d56c646c81c6836b4fd6ddb66f99354b95d33d
Zach <i@ssstttar.com>
2024-11-10 20:28:30 +0800

    feat: MiniLPA 1.1.1
    Signed-off-by: Zach <i@ssstttar.com>

--- Casks/minilpa.rb
+++ Casks/minilpa.rb
@@ -1,9 +1,9 @@
 cask "minilpa" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.1.0"
-  sha256 arm:   "C40D23CD6BE72C36757CAC883F1EBA27AEA8DADA1D67334B9A365791C281CD9A",
-         intel: "E130FB021ACA5794CBA7E676099E390F8C5179F75711B3AD33AA5DBA469A3A43"
+  version "1.1.1"
+  sha256 arm:   "e88982c3880646550a1cc275ea8ff4c78231cf9dadd764128ccc61c7d3a8f1d6",
+         intel: "59e04dbdbf47a717bfd140d388bd4255797cfff6b1f76ea8d0ec67066b0119b1"
 
   url "https://github.com/EsimMoe/MiniLPA/releases/download/v#{version}/MiniLPA-macOS-#{arch}.dmg"
   name "MiniLPA"
