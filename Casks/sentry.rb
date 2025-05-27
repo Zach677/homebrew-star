fa55217ba8a8615730daef2f89c19a6d368e0c41
Zach <2801818616z@gmail.com>
2025-05-27 22:11:46 +0800

    chore: sentry
    Signed-off-by: Zach <2801818616z@gmail.com>

--- Casks/sentry.rb
+++ Casks/sentry.rb
@@ -1,8 +1,8 @@
 cask "sentry" do
-  version "1.2.9"
-  sha256 "f4924226bddd89256c86cc03704c9b9869d76c67490676370a87fda7cb258987"
+  version "1.3.1"
+  sha256 "2e2a0204c8107d5642c94805bbcbf4bd4dd5b03489ee205a20776fd02c03d743"
 
-  url "https://github.com/Lakr233/Sentry/releases/download/#{version}/Sentry-v#{version}.zip"
+  url "https://github.com/Zach677/Sentry/releases/download/#{version}/Sentry.dmg"
   name "Sentry"
   desc "A powerful security monitoring application that detects unauthorized access attempts and records video evidence for you."
   homepage "https://github.com/Lakr233/Sentry"
