d18326fc5296dd83dbd3afcedcbfb3a825fdb6ed
Zach <2801818616z@gmail.com>
2025-06-26 13:11:35 +0800

    feat: flowdown(new cask)

---
+++ Casks/flowdown.rb
@@ -0,0 +1,28 @@
+cask "flowdown" do
+  version "1.18.180"
+  sha256 "4c5b75b1c2af0b63f390ee89e9e72131dec3b821e8db151e1cf9f4b62d4eedc3"
+
+  url "https://github.com/Zach677/NotchDrop/releases/download/#{version}/FlowDown-v#{version}.zip"
+  name "FlowDown"
+  desc "FlowDown is a fast, user-friendly AI agent app for macOS."
+  homepage "https://flowdown.ai/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :ventura"
+
+  app "FlowDown.app"
+
+  preflight do
+    system_command "xattr",
+                   args: ["-cr", "#{staged_path}/FlowDown.app"]
+  end
+
+  # zap trash: [
+  #   "~/Library/Application Scripts/wiki.qaq.FlowDown",
+  #   "~/Library/Containers/wiki.qaq.FlowDown",
+  # ]
+end
