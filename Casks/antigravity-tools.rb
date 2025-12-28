ef19b622f90d5e802f148a900c0dd2541ad8be67
Zach <i@zaxh.org>
2025-12-28 08:43:22 +0000

    antigravity-tools 3.3.0

--- Casks/antigravity-tools.rb
+++ Casks/antigravity-tools.rb
@@ -1,5 +1,5 @@
 cask "antigravity-tools" do
-  version "3.2.1"
+  version "3.3.0"
   sha256 :no_check
 
   url "https://github.com/lbjlaq/Antigravity-Manager/releases/download/v#{version}/Antigravity.Tools_#{version}_universal.dmg"
