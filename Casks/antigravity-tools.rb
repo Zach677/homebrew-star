761ab2a6c31d20fbc5b086fca0295682e6fe0891
ZachTestBot <159118029+ZachTestBot@users.noreply.github.com>
2026-01-06 19:57:40 +0800

    antigravity-tools 3.3.15 (#50)

--- Casks/antigravity-tools.rb
+++ Casks/antigravity-tools.rb
@@ -1,6 +1,6 @@
 cask "antigravity-tools" do
-  version "3.3.5"
-  sha256 "d1e9ae4b61f1b96d14a658d943d7d018b2b9f29ee35ff79d5892ed0b515c2ab7"
+  version "3.3.15"
+  sha256 "81d61ee3ab8127ff5e8325fd32cb561f3f2a10143f2a79e79a7b703cc24e1567"
 
   url "https://github.com/lbjlaq/Antigravity-Manager/releases/download/v#{version}/Antigravity.Tools_#{version}_universal.dmg"
   name "Antigravity Tools"
