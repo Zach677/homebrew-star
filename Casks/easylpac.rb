c2b0deba544cd2645a5bc98a020027d9dc6bb9c4
ZachTestBot <159118029+ZachTestBot@users.noreply.github.com>
2026-03-02 16:51:08 +0800

    easylpac 0.7.9.2 (#93)

--- Casks/easylpac.rb
+++ Casks/easylpac.rb
@@ -1,9 +1,9 @@
 cask "easylpac" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.7.8.4"
-  sha256 arm:   "292b9474b917c559d48365f31ef1bc5e45e3c53a7af799dae92dffddfe9e5090",
-         intel: "9e19f71add70a33fc58259089ae9f6b8cf027c6a67e38cc405232fc4bf72c282"
+  version "0.7.9.2"
+  sha256 arm:   "5ddb2a3e36ad54c56ec0cd08b93cba19664231c81c12eb546574e62ce45f2637",
+         intel: "17cf616a016ea20b627d80017ad869660144f429fe9a9d95b2d3f45c3ff4b9bd"
 
   url "https://github.com/creamlike1024/EasyLPAC/releases/download/#{version}/EasyLPAC-macOS-#{arch}-with-lpac.dmg"
   name "EasyLPAC"
