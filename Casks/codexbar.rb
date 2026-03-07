63bf294a4ea55ce9eba8d26a513e0285100a2431
Zach <2801818616z@gmail.com>
2026-03-07 21:31:45 +0800

    Add CodexBar cask configuration

---
+++ Casks/codexbar.rb
@@ -0,0 +1,38 @@
+cask "codexbar" do
+  version "0.18.0-beta.3"
+  sha256 "3806e9b8c1c6c12b8c7d2e60b6bcaff3e677bc39ec40dade8fc4ee9de96568fd"
+
+  url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBar-#{version}.zip",
+      verified: "github.com/steipete/CodexBar/"
+  name "CodexBar"
+  desc "Menu bar usage monitor for Codex and Claude"
+  homepage "https://codexbar.app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "CodexBar.app"
+  binary "#{appdir}/CodexBar.app/Contents/Helpers/CodexBarCLI", target: "codexbar"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.steipete.codexbar",
+    "~/Library/Application Scripts/com.steipete.codexbar.widget",
+    "~/Library/Application Support/CodexBar",
+    "~/Library/Application Support/com.steipete.codexbar",
+    "~/Library/Caches/CodexBar",
+    "~/Library/Caches/com.steipete.codexbar",
+    "~/Library/Containers/com.steipete.codexbar",
+    "~/Library/Containers/com.steipete.codexbar.widget",
+    "~/Library/Group Containers/group.com.steipete.codexbar",
+    "~/Library/HTTPStorages/com.steipete.codexbar",
+    "~/Library/HTTPStorages/com.steipete.codexbar.binarycookies",
+    "~/Library/Preferences/com.steipete.codexbar.plist",
+    "~/Library/Saved Application State/com.steipete.codexbar.savedState",
+    "~/Library/WebKit/com.steipete.codexbar",
+  ]
+end
